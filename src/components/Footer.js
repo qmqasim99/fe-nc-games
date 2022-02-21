ee3f63ad0b6638ce91c8590dbce220a074cfcb2e
qmqasim99 <Qmqasim99@gmail.com>
2022-02-21 13:49:04 +0000

    Footer link changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,15 @@ const Footer = () => {
         This App is developed using React.js, Axios and REST APIs provided by
         Northcoders
       </p>
+      <p>
+        Another version os this app has been{' '}
+        <a href="https://fe-nc-games-grid.vercel.app/reviews" target="_blank">
+          {' '}
+          deployed on Vercel,
+        </a>{' '}
+        showcasing the design of the user interface using Grid layout and
+        Bootstrap{' '}
+      </p>
     </footer>
   );
 };
